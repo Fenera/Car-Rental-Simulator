10ec3b69184de53cb63f6a2c647e1ae2c74ddfcc
Fenera Taye <fenerataye@gmail.com>
2025-12-03 13:47:23 -0600

    added notes

--- main/java/utilities/Logger.java
+++ main/java/utilities/Logger.java
@@ -5,5 +5,13 @@ package utilities;
 // write any update (car rented out, car returned, staff sign-in, client sign-in)
 
 public class Logger {
+    /*
+    * Attributes:
+    * Title (i.e. New Rental, Check-in, Maintenance Log updated...)
+    * Message
+    *Time
+    * Who: Client or staff updated the log -> name, customerID, staffID, phone number
+    * output file: .txt file
+    * */
 
 }
