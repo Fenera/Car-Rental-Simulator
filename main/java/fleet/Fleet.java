f1959c5909a92f0c09537ce2935307eb3a4ccf9a
Fenera Taye <fenerataye@gmail.com>
2025-12-04 01:57:36 -0600

    started adding vehicles to LL

--- main/java/fleet/Fleet.java
+++ main/java/fleet/Fleet.java
@@ -7,6 +7,7 @@ import utilities.Logger;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 // this stores all the cars
 // get the cars the csv file (use the CSVLoader.java to get the values and format...)
@@ -21,7 +22,8 @@ public class Fleet {
     private LinkedList<Vehicle> availableVehicleLL; // this is to store all the cars that are available
 
     public Fleet(){
-        this.vehicleByVinBST = new BinarySearchTree();
+        this.vehicleByVinBST = new BinarySearchTree<>();
+        this.availableVehicleLL = new LinkedList<Vehicle>();
     }
 
     public void loadFromFile(String file) throws IOException {
@@ -29,27 +31,60 @@ public class Fleet {
         List<String[]> vehicles = reader.readAll();
 
         for(String[] r: vehicles){ // iterate through the rows
+
+            // general vehicle attributes
             // get data from file and store in variables
-            String manufacturer = r[1];
+            int year = 2025;
+            String manufacturer = r[1]; // r[0] is row number so start at r[1]
             String model = r[2];
             String engine = r[3];
             int horsepower = Integer.valueOf(r[4]);
-            int topSpeedMPG = (int) (Integer.valueOf(r[5]) * 0.621371); // convert kmh to mph and convert to int
+            int topSpeedMPH = (int) (Integer.valueOf(r[5]) * 0.621371); // convert kmh to mph and convert to int
             double zeroSixty = Double.valueOf(r[6]);
             int priceMSRP = Integer.valueOf(r[7]);
-            String powertrain = r[8];
+            String fuel = r[8];
             int numberOfSeats = Integer.valueOf(r[9]);
             String vehicleClass = r[10]; // SUV, Sedan..
             double fuelEconomy = Double.valueOf(r[11]);
             double gasTankSize = Double.valueOf(r[12]);
             int vinNumber = Integer.valueOf(r[13]);
+            int inventory = Integer.valueOf(r[14]);
+            int odometer = Integer.valueOf(r[15]);
+            String color = r[16];
+            int numberDoors = Integer.valueOf(r[17]);
+            boolean awd = r[18].equals("TRUE");
+            String fuelClass = r[19];
 
-            vehicleByVinBST.insert(Integer.valueOf(r[r.length - 1])); // VIN is the last column in the array
+
+            // Update in the future (if needed)
+            String conditionReport = "";
+
+            // vehicle class-specific attributes (i.e. bed length for trucks)
+
+            double cargoCapacity = Double.valueOf(r[20]);
+            double towingCapacity = Double.valueOf(r[21]);
+            int standingCapacity = Integer.valueOf(r[22]);
+            int length = Integer.valueOf(r[23]);
+            boolean accessibleRamp = r[24].equals("TRUE");
+            double trunkVolume = Double.valueOf(r[25]);
+            double bedLength = Double.valueOf(r[26]);
+
+
+            vehicleByVinBST.insert(Integer.valueOf(vinNumber)); // VIN is the last column in the array
+            Vehicle vehicle; // instantiate Vehicle object
+
+            // determine the class of the vehicle to create appropriate object (SUV, Sedan...)
+            if(vehicleClass.equals("SUV")){
+                if(Objects.equals(fuelClass, "Gas")) {
+                    vehicle = new SUV(vinNumber, manufacturer, model, year, odometer, color,
+                            numberOfSeats, conditionReport, numberDoors, awd,
+                            new GasMotor(fuel, fuelEconomy, engine, gasTankSize), horsepower, cargoCapacity, towingCapacity);
+                }
+            }
 
             /*
-            * TODO:
-            *  Add vehicles by vin to BST
-            * Add Vehicle objects to LL
+            * TODO: Add Vehicle objects to LL
+            *  else gas, plugIN, hybrid, electric....
             * */
         }
     }
