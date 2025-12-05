a1369e1d0d66b623fce6d611cd1248ea09707079
Fenera Taye <fenerataye@gmail.com>
2025-12-05 00:51:28 -0600

    modified class

--- main/java/datastructures/Queue.java
+++ main/java/datastructures/Queue.java
@@ -2,7 +2,6 @@ package datastructures;
 
 // First In/First Out
 // should use to store reservations
-// could also use for maintenance tasks (maybe use priority queue...?)
 
 public class Queue {
 
