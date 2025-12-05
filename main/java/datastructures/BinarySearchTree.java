14e64fa01082f6ac87e924c7a66f0d14aaa2a691
Fenera Taye <fenerataye@gmail.com>
2025-12-05 00:01:58 -0600

    Added new methods and extra parameter

--- main/java/datastructures/BinarySearchTree.java
+++ main/java/datastructures/BinarySearchTree.java
@@ -8,70 +8,81 @@ package datastructures;
 // NOTE: values in a BST must be unique (in this case)
 /*
 * Source accessed: https://stackoverflow.com/questions/11263244/java-how-do-i-implement-a-generic-binary-search-tree
-* Date: 12/2/2025*/
+* Date: 12/2
+*
+* Source accessed: https://stackoverflow.com/questions/47510048/printing-java-binary-search-tree
+* Date: 12/4
+* */
 
-public class BinarySearchTree <T, V>{
+public class BinarySearchTree <T extends Comparable<T>, V> {
 
     // create Node class
     // T must extend Number to implement comparison
-    class Node<T extends Number> {
-        private T value;
-        private Node<T> left;
-        private Node<T> right;
+    class Node<T extends Comparable<T>, V> {
+        private T key;
+        private V value;
+        private Node<T, V> left;
+        private Node<T, V> right;
+
+        public Node(T key, V value) {
+            this.key = key;
+            this.value = value;
+        }
 
         // methods
 
-        public Node(T value) {
-            this.value = value;
+        public T getKey() {
+            return key;
         }
 
-        public T getValue() {
+        public void setKey(T key) {
+            this.key = key;
+        }
+
+        public V getValue() {
             return value;
         }
 
-        public void setValue(T value) {
+        public void setValue(V value) {
             this.value = value;
         }
 
-        public Node<T> getLeft() {
+        public Node<T, V> getLeft() {
             return left;
         }
 
-        public void setLeft(Node<T> newLeft) {
-            left = newLeft;
+        public void setLeft(Node<T, V> left) {
+            this.left = left;
         }
 
-        public Node<T> getRight() {
+        public Node<T, V> getRight() {
             return right;
         }
 
-        public void setRight(Node<T> newRight) {
-            right = newRight;
+        public void setRight(Node<T, V> right) {
+            this.right = right;
         }
     }
 
-        private Node root; // the root is the top-most node of a BST
+        private Node<T, V> root; // the root is the top-most node of a BST
 
         // no BST constructor is needed because I can assign a root in insert so default constructor suffices
 
-        public <T> boolean insert(T value){
-            Node newNode = new Node((Number) value);
+        public <T> boolean insert(T key, V value){
+            Node newNode = new Node((Comparable) key, value);
 
             // assign root if BST is empty
             if(root == null){
                 root = newNode;
             }
-
-            try {
-                // check if new value is already present in the BST
+                // check if new key is already present in the BST
                 Node temp = root; // temporary Node to serve as an iterator
-                // NOTE: temp is a pointer to root not reference
+                // NOTE: temp is a pointer to root
                 while (true) {
-                    if (newNode.value == temp.value) {
-                        return false;
+                    if (newNode.key == temp.key) {
+                        return false; // key already in BST
                     }
-
-                    if ((int) newNode.value < (int) temp.value) { // newNode is less than temp (to the left of)
+                    if ((int) newNode.key < (int) temp.key) { // newNode is less than temp (to the left of)
                         if (temp.left == null) {
                             temp.left = newNode; // temp.left is empty so put newNode there
                             return true;
@@ -85,26 +96,53 @@ public class BinarySearchTree <T, V>{
                         temp = temp.right; // if temp.right is not empty, shift temp to the right node
                     }
                 }
-            }catch(Exception e){
-                System.out.println("BST can only contain integer values");
-            }
-            return false; // to resolve missing return statement error
         }
 
-        // checks if the binary search tree contains the value
-        public <T> boolean contains(T value){
+        // checks if the binary search tree contains the key (i.e. Vin number)
+        public <T, V> boolean contains(T key){
             if(root == null) return true; // BST is empty
             Node temp = root; // temporary variable that points to root
             while(temp != null){
-                // value is larger than temp's value (to the right of)
-                if((int) value > (int) temp.value){ // convert generic to int for statement to work
+                // key is larger than temp's key (to the right of)
+                if((int) key > (int) temp.key){ // convert generic to int for statement to work
                     temp = temp.right; // shift temp to right node
-                } else if ((int) value < (int) temp.value){ // value is less thhan temp (to the left of)
+                } else if ((int) key < (int) temp.key){ // key is less than temp key(to the left of)
                     temp = temp.left; // shift temp to the left;
-                } else{ // value is not less than/more than temp so it is EQUAL
+                } else{ // key is not less than/more than temp so it is EQUAL
                     return true;
                 }
             }
-            return false; // BST did not contain value
+            return false; // BST did not contain key
         }
+
+        public V searchByKey(T key){
+            if(root == null) return null; // BST is empty
+            Node temp = root; // temporary variable that points to root
+            while(temp != null){
+                // key is larger than temp's key (to the right of)
+                if((int) key > (int) temp.key){ // convert generic to int for statement to work
+                    temp = temp.right; // shift temp to right node
+                } else if ((int) key < (int) temp.key){ // key is less than temp key(to the left of)
+                    temp = temp.left; // shift temp to the left;
+                } else{ // key is not less than/more than temp so it is EQUAL
+                    return (V) temp.value;
+                }
+            }
+            return null;
+        }
+
+        public Node<T, V> getRoot() {
+            return root;
+        }
+
+        public void printTree(Node node){
+                if(node == null) return; // base case
+
+                printTree(node.left); // print left subtree
+                System.out.println(node.value); // print the current nodes
+                printTree(node.right); // print right subtree
+            }
+
+
+
 }
