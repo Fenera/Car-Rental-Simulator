00b798e65464c026303eef153d42dcfa81d24f28
Fenera Taye <fenerataye@gmail.com>
2025-12-06 19:01:43 -0600

    minor changes

--- main/java/staff/Employee.java
+++ main/java/staff/Employee.java
@@ -2,13 +2,26 @@ package staff;
 
 public class Employee extends Staff {
 
+    private boolean isActive; // employee status (true = working)
 
-    public Employee(String name, String email, String phoneNumber, String address, int staffID) {
+    public Employee(String name, String email, String phoneNumber,
+                    String address, int staffID, boolean isActive) {
         super(name, email, phoneNumber, address, staffID);
+        this.isActive = isActive;
     }
 
 
     public void showInventory() {}
+
+    // setter/getter
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
 
     @Override
     public String getInformation() {
